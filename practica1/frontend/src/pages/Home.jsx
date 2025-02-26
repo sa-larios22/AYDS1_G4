3fbcda869702aeb510bdfd580c57b3f9ad5fb707
Alexander Salazar <1817571250101@ingenieria.usac.edu.gt>
2025-02-25 22:42:45 -0600

    new interface_201020600

---
+++ practica1/frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+// src/pages/Home.jsx
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Bienvenido a Mi App</h2>
+      <p>Selecciona una opción del menú.</p>
+    </div>
+  );
+};
+
+export default Home;
